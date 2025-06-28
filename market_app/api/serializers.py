ecd97176f3763dc0f44c77c487d92f116a8cc38c
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-06-28 20:13:30 +0200

    serializer

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -14,23 +14,29 @@ def validate_no_x(value):
     return value
 
 
-class MarketSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    location = serializers.CharField(max_length=255, validators=[validate_no_x])
-    description = serializers.CharField()
-    net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
+class MarketSerializer(serializers.ModelSerializer):
+    # id = serializers.IntegerField(read_only=True)
+    # name = serializers.CharField(max_length=255)
+    # # location = serializers.CharField(max_length=255, validators=[validate_no_x])
+    # location = serializers.CharField(max_length=255)
+    # description = serializers.CharField()
+    # net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
 
-    def create(self, validated_data):
-        return Market.objects.create(**validated_data)
+    # def create(self, validated_data):
+    #     return Market.objects.create(**validated_data)
     
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.location = validated_data.get('location', instance.location)
-        instance.description = validated_data.get('description', instance.description)
-        instance.net_worth = validated_data.get('net_worth', instance.net_worth)
-        instance.save()
-        return instance
+    # def update(self, instance, validated_data):
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.location = validated_data.get('location', instance.location)
+    #     instance.description = validated_data.get('description', instance.description)
+    #     instance.net_worth = validated_data.get('net_worth', instance.net_worth)
+    #     instance.save()
+    #     return instance
+    
+    class Meta:
+        model = Market
+        # fields = ('id', 'name', 'location', 'description', 'net_worth')
+        field = '__all__'
     
     # def validate_location(self, value):
     #     if 'X' in value:
